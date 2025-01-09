867569005c53a350ff649fb97b928711afee29de
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-09 15:43:05 +0000

    Add base.html

--- booknook/settings.py
+++ booknook/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-debbiecoates-booknook-xxnivdbr0oe.ws.codeinstitute-ide.net', 'booknook2025-68fc319d56ec.herokuapp.com']
 
