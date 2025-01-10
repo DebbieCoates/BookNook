6a6a37b20d1f71b57d706ddc59737ef5784aa05d
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-10 17:50:10 +0000

    add allauth

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,13 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    # Link to Homepage
+     # Link to Homepage
     path('', views.HomePage.as_view(), name='home'),
+
+    #Link to accounts
+    path("accounts/", include("allauth.urls")),
+
+
 
 ]
